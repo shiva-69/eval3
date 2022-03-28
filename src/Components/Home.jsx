d050b5e5806aa82e13c488058ae272e47cefbd6e
shiva-69 <92380211+shiva-69@users.noreply.github.com>
2022-03-28 22:07:26 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {useNavigate} from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div><h1>
       HomePage
+      
       </h1>
       </div>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,17 +5,23 @@ import styled from 'styled-components'
 const NavbarWrapper = styled.div`
 display :flex;
 flex-direction : row;
+align-items : center;
 justify-content : center;
-
+background-color : #cecece;
+color : white;
 `
+const LinkWrapper = styled.div`
+margin : 5px`
 const Navbar = () => {
   return (
-    <NavbarWrapper>
-        <Link to="/">Home</Link>
-        <Link to="/Login">Login</Link>
-        <Link to ="/Register"> Register</Link>
-        <Link to ="/Products"> Products</Link>
+    <div>
+      <NavbarWrapper>
+      <LinkWrapper> <Link to="/">Home</Link></LinkWrapper>
+      <LinkWrapper> <Link to="/Login">Login</Link></LinkWrapper>
+      <LinkWrapper>  <Link to ="/Register"> Register</Link></LinkWrapper>
+      <LinkWrapper> <Link to ="/Products"> Products</Link></LinkWrapper>
     </NavbarWrapper>
+    </div>
   )
 }
 
